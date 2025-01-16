cebf72dbeaf372b452bbd48df304d06fde9663aa
Haojian Wu <hokein.wu@gmail.com>
2025-01-16 16:52:01 +0100

    Simplify the testcase.

--- uninit-122493/set_bits2.h
+++ uninit-122493/set_bits2.h
@@ -1,17 +1,15 @@
 #pragma once
 
-#include "string_view2.h"
+
+template <typename T>
+void fwd(const T& x);
+
 
 namespace vox::bitset {
 
 template <typename TFunc>
-void ForEachWord2(const TFunc& func) {
-  fwd<TFunc>(func);
-}
-
-template <typename TFunc>
-void ForEachSetBit2(const TFunc& func) {
-  ForEachWord2([](int) {
+void ForEachSetBit2(const TFunc&) {
+  fwd([](int) {
     const int bit_index_base = 0;
     (void)[&](int) {
       int v = bit_index_base;
--- uninit-122493/string_view2.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-
-template <typename T>
-const T& fwd(const T& x) {
-  return x;
-}
-
